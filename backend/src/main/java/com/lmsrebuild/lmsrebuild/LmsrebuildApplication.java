94c858d24e80cb1914eef039ef395e7a2037e94f
bdhamithkumara <damithkumararoxx55@gmail.com>
2023-04-11 12:42:13 +0530

    startbackend

---
+++ backend/src/main/java/com/lmsrebuild/lmsrebuild/LmsrebuildApplication.java
@@ -0,0 +1,14 @@
+package com.lmsrebuild.lmsrebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsrebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsrebuildApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
