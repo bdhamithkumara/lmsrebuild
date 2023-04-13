26deca8be785e6239726b68436c1bd9c3393d1f7
bdhamithkumara <damithkumararoxx55@gmail.com>
2023-04-13 21:30:39 +0530

    create user admin pannel

--- frontend/src/components/ACretateUAccDetails.jsx
+++ frontend/src/components/ACretateUAccDetails.jsx
@@ -1,32 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Paper } from '@mui/material';
 
 const ACretateUAccDetails = () => {
-  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [type, setType] = useState("");
+  const [alluserlogindata,setAlluserlogindata] = useState([]);
 
   const handleSubmit = (e) => {
-    
+    e.preventDefault();
+    const alluserlogin = {username,password,type};
+    console.log(alluserlogin);
+    fetch("http://localhost:8080/logindata/add",{
+      method:"POST",
+      headers:{"content-Type":"application/json"},
+      body:JSON.stringify(alluserlogin)
+    }).then(()=>{
+      console.log("data added");
+    })
+    setUsername("");
+    setPassword("");
+    setType();
   }
+
+  useEffect(() => {
+    fetch("http://localhost:8080/logindata/getAll")
+    .then(res=>res.json())
+    .then((result)=>{
+      setAlluserlogindata(result);
+    })
+  },[]);
 
   return (
     <div>
       <div className="flex">
         <div className="basis-3/4">
           <form>
-            <label class="block">
+            <label className="block">
               <input
                 type="email"
                 name="email"
-                class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#F1B81F] focus:ring-[#F1B81F] block w-full rounded-md sm:text-sm focus:ring-1"
+                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#F1B81F] focus:ring-[#F1B81F] block w-full rounded-md sm:text-sm focus:ring-1"
                 placeholder="username"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
               />
               <input
-                type="email"
-                name="email"
-                class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#F1B81F] focus:ring-[#F1B81F] block w-full rounded-md sm:text-sm focus:ring-1"
+                type="password"
+                name="password"
+                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-[#F1B81F] focus:ring-[#F1B81F] block w-full rounded-md sm:text-sm focus:ring-1"
                 placeholder="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -45,14 +67,22 @@ const ACretateUAccDetails = () => {
                 </select>
               </div>
 
-              <button class="rounded-full bg-[#F1B81F] text-black p-2 mt-5" onClick={handleSubmit}>
-                Save Changes
+              <button className="rounded-full bg-[#F1B81F] text-black p-2 mt-5" onClick={handleSubmit}>
+                Save 
               </button>
             </label>
           </form>
         </div>
         <div className="basis-1/4">
-          <div className="px-2"></div>
+          <div className="px-2">
+            {alluserlogindata.map(Alluserlogindata=>(
+                              <Paper elevation={6} style={{margin:"10px",padding:"15px",textAlign:"left"}} key={Alluserlogindata.id}>
+
+                              ID : {Alluserlogindata.id} &nbsp; &nbsp; 
+                              USERNAME : {Alluserlogindata.username}
+                          </Paper>
+            ))}
+          </div>
         </div>
       </div>
     </div>
