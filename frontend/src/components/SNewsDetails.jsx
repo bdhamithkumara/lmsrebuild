752c50b6a8d232ef36e8e3409da1a3f3fd24e9ee
bdhamithkumara <damithkumararoxx55@gmail.com>
2023-04-14 12:15:41 +0530

    retrive news to student dashboard

--- frontend/src/components/SNewsDetails.jsx
+++ frontend/src/components/SNewsDetails.jsx
@@ -1,91 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Nimge from "../images/newsimg.png";
+import { useState } from "react";
 
 const SNewsDetails = () => {
+  const [allnews, setAllnews] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/news/alldata")
+      .then((res) => res.json())
+      .then((result) => {
+        setAllnews(result);
+      });
+  }, []);
+
   return (
     <div>
       <div className="mt-8 mx-8">
-        <div className="bg-[#3B1234]  rounded-2xl text-white my-5">
-          <div className="flex">
-            <div className="p-5">
-              <img
-                src={Nimge}
-                alt="newsimage"
-                className="w-[300px] h-[150px] rounded-2xl "
-              />
-            </div>
-            <div className="flex-col mt-4">
-              <div className="text-3xl">
-                Sri Lanka : President opens University of Vavuniya
+        {allnews.map((viewallnew) => (
+          <div className="bg-[#3B1234]  rounded-2xl text-white my-5" key={viewallnew.id}>
+            <div className="flex">
+              <div className="p-5">
+                <img
+                  src={Nimge}
+                  alt="newsimage"
+                  className="w-[300px] h-[150px] rounded-2xl "
+                />
               </div>
-              <div className="mt-2 pr-5">
-                <p>
-                  Feb 11, Colombo: President Gotabaya Rajapaksa Friday (11)
-                  declared the Vavuniya Campus, affiliated to the Jaffna
-                  University, as a National
-                </p>
-              </div>
-              <div className="mt-3">
-                <div>24th March 2021</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="bg-[#3B1234]  rounded-2xl text-white my-5">
-          <div className="flex">
-            <div className="p-5">
-              <img
-                src={Nimge}
-                alt="newsimage"
-                className="w-[300px] h-[150px] rounded-2xl "
-              />
-            </div>
-            <div className="flex-col mt-4">
-              <div className="text-3xl">
-                Sri Lanka : President opens University of Vavuniya
-              </div>
-              <div className="mt-2 pr-5">
-                <p>
-                  Feb 11, Colombo: President Gotabaya Rajapaksa Friday (11)
-                  declared the Vavuniya Campus, affiliated to the Jaffna
-                  University, as a National
-                </p>
-              </div>
-              <div className="mt-3">
-                <div>24th March 2021</div>
+              <div className="flex-col mt-4">
+                <div className="text-3xl">
+                  {viewallnew.newstitle}
+                </div>
+                <div className="mt-2 pr-5">
+                  <p>
+                  {viewallnew.content}
+                  </p>
+                </div>
+                <div className="mt-3">
+                  <div>
+                  {viewallnew.today}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="bg-[#3B1234]  rounded-2xl text-white my-5">
-          <div className="flex">
-            <div className="p-5">
-              <img
-                src={Nimge}
-                alt="newsimage"
-                className="w-[300px] h-[150px] rounded-2xl "
-              />
-            </div>
-            <div className="flex-col mt-4">
-              <div className="text-3xl">
-                Sri Lanka : President opens University of Vavuniya
-              </div>
-              <div className="mt-2 pr-5">
-                <p>
-                  Feb 11, Colombo: President Gotabaya Rajapaksa Friday (11)
-                  declared the Vavuniya Campus, affiliated to the Jaffna
-                  University, as a National
-                </p>
-              </div>
-              <div className="mt-3">
-                <div>24th March 2021</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        
-        
+        ))}
       </div>
     </div>
   );
